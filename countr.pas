2fff56e0f9cdcf483b2a77a263eb7a01b89bc410
Matthew Hipkin <hippy2094@users.noreply.github.com>
2015-05-13 11:15:41 +0100

    Some polishing

--- countr.pas
+++ countr.pas
@@ -1,7 +1,7 @@
 (* Recursively count files in a directory *)
 program countr;
 {$mode objfpc}{$H+}
-uses Classes, SysUtils;
+uses Classes, SysUtils, StrUtils;
 
 var
   fcount: LongInt;
@@ -27,14 +27,40 @@ begin
       end;
     until FindNext(s) <> 0;
   end;
+  FindClose(s);
 end;
 
+function main: Integer;
+var
+  mask, inDir: String;
 begin
   fcount := 0;
+  Result := 0;
   
   // ParamStr(1) = path
   // ParamStr(2) = mask
- 
-  DoCount(ParamStr(2),ParamStr(1));
-  writeln(fcount, ' file(s) found.');
+
+  // Directory doesn't exist?
+  if not DirectoryExists(ParamStr(1)) then Result := 1
+  else inDir := ParamStr(1);
+  // No mask specified?
+  if ParamCount = 1 then mask := '*.*'
+  else mask := ParamStr(2);
+  // Bash appears to expand filemasks into actual parameters
+  if not AnsiStartsStr('*',ParamStr(2)) then
+  begin
+    mask := '*' + ExtractFileExt(ParamStr(2));
+  end;
+  // No parameters?
+  if ParamCount = 0 then Result := 2;
+
+  if Result < 1 then DoCount(mask,inDir);
+end;
+
+begin
+  case main of
+    0: writeln(fcount, ' file(s) found.');
+    1: writeln('ERROR: Directory not found.');
+    2: writeln('USAGE: countr <path> [searchmask]');
+  end;
 end.
